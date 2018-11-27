fc6bad4456702b024137c5222d235f0f00bd0f3d
student <student@itstep1.dp.ua>
2018-11-27 14:43:13 +0200

    DataBaseContext

---
+++ Library/Library/DataBaseContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Lib
+{
+    class DataBaseContext:DbContext
+    {
+        public DataBaseContext() : base("connStr") { }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Edition> Editions { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Type> Types { get; set; }
+        public DbSet<BookAndReader> BookAndReaders { get; set; }
+    }
+}
