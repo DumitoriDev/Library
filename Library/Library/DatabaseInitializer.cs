03032301830141942374b815b6e842e54c826a9d
Dmitry <blackmesa2018@gmail.com>
2018-11-27 15:47:16 +0300

    Add entity

---
+++ Library/Library/DatabaseInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class DatabaseInitializer: DropCreateDatabaseIfModelChanges<DatabaseContext>
+    {
+        protected override void Seed(DatabaseContext ctx)
+        {
+            //var tmpCategory = new Category
+            //{
+            //    Name = "Milk products",
+            //    Products = new List<Product>
+            //    {
+            //        new Product
+            //        {
+            //            Name = "Milk",
+            //            Price = 100
+            //        },
+            //        new Product
+            //        {
+            //            Name = "Kefir",
+            //            Price = 70
+            //        }
+
+            //    }
+
+            //};
+
+            //ctx.Entry(tmpCategory).State = EntityState.Added;
+            //ctx.SaveChanges();
+        }
+    }
+}
