65f1bd0fec53087998af7d3056663f6e66a10328
DumitoriDev <blackmesa2018@gmail.com>
2018-11-30 13:13:16 +0300

    add other files from project

---
+++ Library/Library/Library/DatabaseContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Library
+{
+    public class DataBaseContext:DbContext
+    {
+        private DataBaseContext() : base("connStr") { }
+        private static DataBaseContext instance;
+        public static DataBaseContext GetInstance()
+        {
+            if(instance==null)
+            {
+                instance = new DataBaseContext();
+            }
+            return instance;
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Edition> Editions { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Type> Types { get; set; }
+        public DbSet<Reader> Readers { get; set; }
+        public DbSet<BookAndReader> BookAndReaders { get; set; }
+    }
+}
---
+++ Library/Library/Library/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    interface IRepository<T> where T:class
+    {
+        T Get(int id);
+        void Update(T item);
+        void Delete(int id);
+        void Add(T item);
+        IEnumerable<T> GetAll();
+    }
+}
